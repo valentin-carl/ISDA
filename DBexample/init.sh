ecee5357143382fd5d29c440b09d9b170fda583e
valentin-carl <vacleantrinl@googlemail.com>
2022-04-24 22:28:36 +0200

    update init.sh

--- DBexample/init.sh
+++ DBexample/init.sh
@@ -1,1 +1,13 @@
-create_db.pyrm init.sh/bin/sh
+#!/bin/sh
+
+# check whether database file exists
+FILE=database.db
+if [[ -f "$FILE" ]]; then
+	echo "$FILE exists."
+else
+	touch database.db
+	echo "created $FILE."
+fi
+
+# fill database with values
+python3 create_db.py
